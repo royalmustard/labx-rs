87a0db4a8441a40b3d95f872c1189f731ace98cf
royalmustard <felix@csruediger.de>
2023-03-30 22:01:01 +0200

    parser working

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,102 @@
-use std::io::Read;
+use std::fmt::Display;
+use std::fs::read;
+use std::path::Path;
+use std::{collections::HashMap, io::Read};
 
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
 
-use serde::{Deserialize, Serialize};
-struct CassyDaten
-{
-    messungen: Vec<Messung>
+
+#[derive(Debug)]
+struct CassyDaten {
+    messungen: Vec<Messung>,
 }
 
-
-struct Messung
-{
+#[derive(Debug)]
+struct Messung {
     zeitpunkt: String,
     beschreibung: String,
-    datenreihen: Vec<Datenreihe>
+    datenreihen: Vec<Datenreihe>,
 }
-#[derive(Deserialize, Debug)]
-#[serde(rename="channel")]
-struct Datenreihe
-{
-    #[serde(rename="values")]
-    werte: Vec<Value>,
-    #[serde(rename="$value")]
-    quantity: String,
-    #[serde(rename="$value")]
-    symbol: String,
-    #[serde(rename="$value")]
-    unit: String
-}
-#[derive(Deserialize, Debug)]
-struct Value
-{
-    #[serde(rename="$value")]
-    wert: f64
-}
-#[derive(Deserialize, Debug, Serialize)]
-struct channel
-{
 
+#[derive(Debug, Clone)]
+struct Datenreihe {
     quantity: String,
     symbol: String,
     unit: String,
-    values: Vec<f64>
+    values: Vec<f64>,
 }
-fn main(){
-    let cassy_daten = CassyDaten{messungen: Vec::new()};
-    // let file = std::fs::read_to_string("data_fat.xml").unwrap();
-    // let mut reader = Reader::from_str(&file);
-    // let mut buf:Vec<u8> = Vec::new();
-    // // loop {
-    //     match reader.read_event_into(&mut buf) {
-    //         Ok(Event::Start(e)) if e.name().as_ref() == b"channel" => {
-    //             if let Ok(Some(_)) = e.try_get_attribute("datetime")
-    //             {
-    //                 let txt = reader.read_text(e.name()).unwrap();
-    //                 println!("{}", txt);
-    //                 let datenreihe: Datenreihe = serde_xml_rs::from_str(&format!("<channel>{}</channel>", txt)).unwrap();
-    //                 println!("{:?}", datenreihe)
-    //             }
-    //         }
-    //         Ok(Event::Eof) => break, // exits the loop when reaching end of file
-    //         Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
-    //         _ => (), // There are several other `Event`s we do not consider here
-    //     }
-    // }
 
-    let test = r##"<channel><quantity>Index</quantity><symbol>n</symbol><unit /><range min="0" max="100001" /><values count="100001"></values></channel>"##;
-    println!("{}", test);
-    let owo: Datenreihe = serde_xml_rs::from_str(test).unwrap();//channel { quantity: "owo".to_string(), symbol: "sus".to_string(), unit: "bus".to_string(), values: vec![420.69f64] };
-    //println!("{}", serde_xml_rs::to_string(&owo).unwrap());
+// impl Display for Vec<f64>
+// {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         let out: String = format!("[{}, ..., {}] ({} values)", self.first(), self.last(), self.len());
+//         write!(f, "{}", out)
+//     }
+// }
+fn main() {
+    let mut cassy_daten = CassyDaten {
+        messungen: Vec::new(),
+    };
+
+
+    let file = std::fs::File::open(Path::new("owo.labx")).expect("Error opening file!");
+    let mut buf = String::new();
+    zip::ZipArchive::new(file).expect("Error parsing zipfile!").by_name("data.xml").expect("data.xml not found in zipfile").read_to_string(&mut buf).expect("Error reading data.xml to buffer");
+    let mut reader = Reader::from_str(&buf);
+
+    let mut buf: Vec<u8> = Vec::new();
+    let mut unit_indices: HashMap<String, usize> = HashMap::new();
+    let mut current_symbol: String = "".to_string();
+    let mut messung: Messung;
+    let mut datenreihe_vec: Vec<f64> = Vec::new();
+    let mut datenreihe: Datenreihe = Datenreihe { values: Vec::new(), quantity: "sus".to_string(), symbol: "bus".to_string(), unit: "ඞ".to_string() };
+    loop {
+        match reader.read_event_into(&mut buf) {
+            Ok(Event::Start(e)) if e.name().as_ref() == b"channel" => {
+                if let Ok(Some(_)) = e.try_get_attribute("datetime") 
+                {
+                    //Erstelle Neue Messung
+                    messung = Messung{zeitpunkt: reader.decoder().decode(e.try_get_attribute("datetime").unwrap().unwrap().value.as_ref()).unwrap().to_string(),
+                                    beschreibung: String::from(""), //sort it out later,
+                                datenreihen: Vec::new()};
+                    cassy_daten.messungen.push(messung);
+
+                }
+                else 
+                {
+                    
+                }
+                if !datenreihe_vec.is_empty()
+                {
+                    datenreihe.values = datenreihe_vec.clone();
+                    cassy_daten.messungen.get_mut(*unit_indices.get(&current_symbol).unwrap()).unwrap().datenreihen.push(datenreihe.clone());
+                    //println!("{:?}", datenreihe);
+                    datenreihe_vec.clear();
+                }
+            }
+            Ok(Event::Start(e)) if e.name().as_ref() == b"quantity" =>
+            {
+                datenreihe.quantity = reader.read_text(e.name()).unwrap().to_string();
+            }
+            Ok(Event::Start(e)) if e.name().as_ref() == b"symbol" =>
+            {
+                current_symbol = reader.read_text(e.name()).unwrap().to_string();
+                datenreihe.symbol = current_symbol.to_string();
+                unit_indices.entry(current_symbol.clone()).and_modify(|idx| *idx = *idx+1).or_insert(0);
+                
+            }
+            Ok(Event::Start(e)) if e.name().as_ref() == b"unit" =>
+            {
+                datenreihe.unit = reader.read_text(e.name()).unwrap().to_string();
+            }
+            Ok(Event::Start(e)) if e.name().as_ref() == b"value" => {
+                datenreihe_vec.push( fast_float::parse(reader.read_text(e.name()).unwrap().as_ref()).unwrap());
+            }
+            Ok(Event::Eof) => break, // exits the loop when reaching end of file
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            _ => (), // There are several other `Event`s we do not consider here
+        }
+    }
+    //println!("{:?}", cassy_daten)
 }
